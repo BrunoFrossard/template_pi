211367c64c0acfbe6c8be460b7a508307610b193
Bruno Frossard <bruno.frossard@sou.inteli.edu.br>
2025-05-08 23:48:50 -0300

    Ajusta estrutura do projeto, remove arquivos desnecessários e atualiza .gitignore

--- template_pi/routes/eventRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Exemplo de rota para listar eventos
-router.get('/', (req, res) => {
-  res.send('Listar eventos');
-});
-
-module.exports = router;
